1f95276d362ae7c60e3c450512d3809927b50442
128Shaheer <128Skhan@frhsd.com>
2026-09-11 09:37:54 -0400

    Second reply

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("NSK");
         System.out.println("Hi NSK, I am CG");
+        System.out.print("Hi NSk and CG, I am SK");
     }
 }
